fae80928d549c55178bf724bb18e32b12a58cc18
AnaghaCS <anaghasasidharan38@gmail.com>
2024-02-24 17:18:57 +0000

    User signup

--- Users/urls.py
+++ Users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     # Add more URL patterns as needed
-#     path('', views.index, name='index'),
+#     path('users_logins',views.users_logins, name = 'users_logins'),
+    path('user_signup',views.user_signup, name ='user_signup'),
+#     path('user_home',views.user_home, name = 'user_home'),
+#     path('user_logout',views.user_logout, name = 'user_logout'),
 ]
