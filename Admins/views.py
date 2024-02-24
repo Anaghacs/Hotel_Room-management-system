5e09d469d7695ae6d560ef4db88d23c4a04d4ad7
AnaghaCS <anaghasasidharan38@gmail.com>
2024-02-24 15:59:57 +0000

    Template setup completed

--- Admins/views.py
+++ Admins/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+from django.contrib.auth import authenticate
+from django.contrib import messages,auth
+from django.contrib.auth import login as auth_login
+# from hotels.models import Hotels
+from django.contrib.auth import logout
+# from users.views import user_home
+# from hotels.views import hotel_login
+# Create your views here.
+
+def index(request):
+      return render(request,'commons/index.html')
